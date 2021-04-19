28463d4b79d3ca189b1c262de3dbdeef0625e41e
zhorlrnaikaub <tangyuanca@163.com>
2021-04-19 16:11:11 +0800

    插入二维码中间头像小图片

--- src/main/java/com/zhangjunqiao/zchz/qqzonegetpics/App3.java
+++ src/main/java/com/zhangjunqiao/zchz/qqzonegetpics/App3.java
@@ -2,8 +2,6 @@ package com.zhangjunqiao.zchz.qqzonegetpics;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -13,18 +11,22 @@ import com.google.zxing.WriterException;
 /**
  * 
  * @author zjq
- * @aim 20150906学习处理二维码
+ * @aim 20150906处理二维码
  * 
  */
 public class App3 {
 
 	public static void main(String[] args) throws WriterException {
-		/*
-		 * String imgPath = "D:\\Users\\Zhangjunqiao\\3_Files"; try {
-		 * BufferedImage image = ImageIO.read(new FileInputStream(imgPath)); }
-		 * catch (FileNotFoundException e) { e.printStackTrace(); } catch
-		 * (IOException e) { e.printStackTrace(); }
-		 */
+
+		// String imgPath = "D:\\Users\\Zhangjunqiao\\3_Files";
+		// try {
+		// BufferedImage image = ImageIO.read(new FileInputStream(imgPath));
+		// } catch (FileNotFoundException e) {
+		// e.printStackTrace();
+		// } catch (IOException e) {
+		// e.printStackTrace();
+		// }
+
 		String content = "http://www.sinaimg.cn/dy/slidenews/8_img/2015_36/65486_439634_724233.jpg";
 
 		BufferedImage image2 = QRCodeUtil.standEncodeQr(content);
@@ -32,6 +34,15 @@ public class App3 {
 			ImageIO.write(image2, "jpg", new File(
 					"D:\\Users\\Zhangjunqiao\\3_Files\\2.jpg"));
 		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		String imgUrl = "http://www.qq1234.org/uploads/allimg/140714/3_140714160149_5.png"; // 小黄人
+		try {
+			BufferedImage image3 = QRCodeUtil.createImage(content, imgUrl);
+			ImageIO.write(image3, "jpg", new File(
+					"D:\\Users\\Zhangjunqiao\\3_Files\\3.jpg"));
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
--- src/main/java/com/zhangjunqiao/zchz/qqzonegetpics/QRCodeUtil.java
+++ src/main/java/com/zhangjunqiao/zchz/qqzonegetpics/QRCodeUtil.java
@@ -1,7 +1,18 @@
 package com.zhangjunqiao.zchz.qqzonegetpics;
 
+import java.awt.BasicStroke;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Shape;
+import java.awt.geom.RoundRectangle2D;
 import java.awt.image.BufferedImage;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Hashtable;
+
+import javax.imageio.ImageIO;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
@@ -13,7 +24,7 @@ import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 /**
- * @date 20150905
+ * 
  * @author zjq
  * @aim 生成二维码工具
  */
@@ -54,8 +65,75 @@ public class QRCodeUtil {
 		return image;
 	}
 
-	/*
-	*//**
+	/**
+	 * 拼接二维码图片
+	 * 
+	 * @param content
+	 * @param imgContent
+	 * @return
+	 * @throws Exception
+	 */
+	public static BufferedImage createImage(String text, String imgUrl)
+			throws Exception {
+
+		// 创建一个标准二维码图片
+		BufferedImage image = standEncodeQr(text);
+
+		// 插入中间小图标
+		insertImage(image, imgUrl);
+		return image;
+	}
+
+	/**
+	 * 插入二维码中间头像小图片
+	 * 
+	 * @param source
+	 * @param imgPath
+	 * @throws Exception
+	 */
+	public static void insertImage(BufferedImage source, String imgUrl)
+			throws Exception {
+
+		URL url = new URL(imgUrl);
+		HttpURLConnection httpURLConnection = (HttpURLConnection) url
+				.openConnection();
+		httpURLConnection.connect();
+		InputStream in = httpURLConnection.getInputStream();
+
+		Image src = ImageIO.read(in);
+		in.close();
+
+		int width = src.getWidth(null);
+		int height = src.getHeight(null);
+
+		if (true) { // 压缩LOGO
+			if (width > WIDTH) {
+				width = WIDTH;
+			}
+			if (height > HEIGHT) {
+				height = HEIGHT;
+			}
+			Image image = src.getScaledInstance(width, height,
+					Image.SCALE_SMOOTH);
+			BufferedImage tag = new BufferedImage(width, height,
+					BufferedImage.TYPE_INT_RGB);
+			Graphics g = tag.getGraphics();
+			g.drawImage(image, 0, 0, null); // 绘制缩小后的图
+			g.dispose();
+			src = image;
+		}
+		Graphics2D graph = source.createGraphics();
+
+		int x = (QRCODE_SIZE - width) / 2;
+		int y = (QRCODE_SIZE - height) / 2;
+		graph.drawImage(src, x, y, width, height, null);
+		Shape shape = new RoundRectangle2D.Float(x, y, width, width, 6, 6);
+		graph.setStroke(new BasicStroke(3f));
+		graph.draw(shape);
+		graph.dispose();
+	}
+
+	/**
 	 * 创建 image 完整二维码图片
 	 * 
 	 * @param content
@@ -63,32 +141,42 @@ public class QRCodeUtil {
 	 * @param response
 	 * @throws Exception
 	 */
-	/*
-	 * public static BufferedImage encode(String text, NamecardVo namecardVo)
-	 * throws Exception {
-	 * 
-	 * BufferedImage image1 = createImage(text, namecardVo.getNamecardInfo()
-	 * .get(NamecardInfoKeyCons.PHOTOURL)); int width = image1.getWidth(); int
-	 * height = image1.getHeight();
-	 * 
-	 * BufferedImage image2 = new BufferedImage(300, 182,
-	 * BufferedImage.TYPE_INT_RGB); int width2 = image2.getWidth(); int height2
-	 * = image2.getHeight();
-	 * 
-	 * // 画图 selfDefinedPhoto(namecardVo, image2);
-	 * 
-	 * // 从图片中读取RGB int[] imageArrayOne = new int[width * height]; imageArrayOne
-	 * = image1.getRGB(0, 0, width, height, imageArrayOne, 0, width); int[]
-	 * imageArrayTwo = new int[width2 * height2]; imageArrayTwo =
-	 * image2.getRGB(0, 0, width2, height2, imageArrayTwo, 0, width2);
-	 * 
-	 * // 生成新图片 BufferedImage imageNew = new BufferedImage(300, 482,
-	 * BufferedImage.TYPE_INT_RGB); imageNew.setRGB(0, 0, width, height,
-	 * imageArrayOne, 0, width);// 设置左半部分的RGB imageNew.setRGB(0, height, width,
-	 * height2, imageArrayTwo, 0, width);// 设置下半部分的RGB
-	 * 
-	 * return imageNew; }
-	 *//**
+	// public static BufferedImage encode(String text, NamecardVo namecardVo)
+	// throws Exception {
+	//
+	// BufferedImage image1 = createImage(text, namecardVo.getNamecardInfo()
+	// .get(NamecardInfoKeyCons.PHOTOURL));
+	// int width = image1.getWidth();
+	// int height = image1.getHeight();
+	//
+	// BufferedImage image2 = new BufferedImage(300, 182,
+	// BufferedImage.TYPE_INT_RGB);
+	// int width2 = image2.getWidth();
+	// int height2 = image2.getHeight();
+	//
+	// // 画图
+	// selfDefinedPhoto(namecardVo, image2);
+	//
+	// // 从图片中读取RGB
+	// int[] imageArrayOne = new int[width * height];
+	// imageArrayOne = image1.getRGB(0, 0, width, height, imageArrayOne, 0,
+	// width);
+	// int[] imageArrayTwo = new int[width2 * height2];
+	// imageArrayTwo = image2.getRGB(0, 0, width2, height2, imageArrayTwo, 0,
+	// width2);
+	//
+	// // 生成新图片
+	// BufferedImage imageNew = new BufferedImage(300, 482,
+	// BufferedImage.TYPE_INT_RGB);
+	// imageNew.setRGB(0, 0, width, height, imageArrayOne, 0, width);//
+	// 设置左半部分的RGB
+	// imageNew.setRGB(0, height, width, height2, imageArrayTwo, 0, width);//
+	// 设置下半部分的RGB
+	//
+	// return imageNew;
+	// }
+
+	/**
 	 * 自定义图片以便拼接到二维码上
 	 * 
 	 * @param namecardVo
@@ -99,75 +187,36 @@ public class QRCodeUtil {
 	 * @throws FontFormatException
 	 * @throws IOException
 	 */
-	/*
-	 * private static void selfDefinedPhoto(NamecardVo namecardVo, BufferedImage
-	 * image) throws FileNotFoundException, FontFormatException, IOException {
-	 * Graphics2D graph = image.createGraphics(); graph.setColor(Color.WHITE);
-	 * graph.fillRect(0, 0, image.getWidth(), image.getHeight()); File file =
-	 * new File("src/main/resources/李旭科漫画体v1.0.ttf"); FileInputStream fis = new
-	 * FileInputStream(file); BufferedInputStream bis = new
-	 * BufferedInputStream(fis);
-	 * 
-	 * Font dynamicFont = Font.createFont(Font.TRUETYPE_FONT, bis).deriveFont(
-	 * Font.BOLD, 24f);
-	 * 
-	 * graph.setColor(Color.RED); graph.setFont(dynamicFont); graph.drawString(
-	 * namecardVo.getNamecardInfo().get(NamecardInfoKeyCons.NICKNAME), 120, 50);
-	 * 
-	 * graph.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-	 * RenderingHints.VALUE_ANTIALIAS_ON); fis.close(); bis.close();
-	 * 
-	 * graph.setColor(Color.GREEN); graph.drawString(
-	 * namecardVo.getNamecardInfo().get(NamecardInfoKeyCons.COUNTRY), 120, 100);
-	 * 
-	 * graph.dispose(); }
-	 *//**
-	 * 拼接二维码图片
-	 * 
-	 * @param content
-	 * @param imgContent
-	 * @return
-	 * @throws Exception
-	 */
-	/*
-	 * private static BufferedImage createImage(String text, String imgUrl)
-	 * throws Exception {
-	 * 
-	 * //创建一个标准二维码图片 BufferedImage image = standEncodeQr(text);
-	 * 
-	 * // 插入中间小图标 insertImage(image, imgUrl); return image; }
-	 *//**
-	 * 插入二维码中间头像小图片
-	 * 
-	 * @param source
-	 * @param imgPath
-	 * @throws Exception
-	 */
-	/*
-	 * private static void insertImage(BufferedImage source, String imgUrl)
-	 * throws Exception { logger.info("photo url {}", imgUrl);
-	 * 
-	 * URL url = new URL(imgUrl); HttpURLConnection httpURLConnection =
-	 * (HttpURLConnection) url .openConnection(); httpURLConnection.connect();
-	 * InputStream in = httpURLConnection.getInputStream();
-	 * 
-	 * Image src = ImageIO.read(in); in.close();
-	 * 
-	 * int width = src.getWidth(null); int height = src.getHeight(null);
-	 * 
-	 * if (true) { // 压缩LOGO if (width > WIDTH) { width = WIDTH; } if (height >
-	 * HEIGHT) { height = HEIGHT; } Image image = src.getScaledInstance(width,
-	 * height, Image.SCALE_SMOOTH); BufferedImage tag = new BufferedImage(width,
-	 * height, BufferedImage.TYPE_INT_RGB); Graphics g = tag.getGraphics();
-	 * g.drawImage(image, 0, 0, null); // 绘制缩小后的图 g.dispose(); src = image; }
-	 * Graphics2D graph = source.createGraphics();
-	 * 
-	 * int x = (QRCODE_SIZE - width) / 2; int y = (QRCODE_SIZE - height) / 2;
-	 * graph.drawImage(src, x, y, width, height, null); Shape shape = new
-	 * RoundRectangle2D.Float(x, y, width, width, 6, 6); graph.setStroke(new
-	 * BasicStroke(3f)); graph.draw(shape); graph.dispose();
-	 * 
-	 * }
-	 */
+	// private static void selfDefinedPhoto(NamecardVo namecardVo,
+	// BufferedImage image)
+	// throws FileNotFoundException, FontFormatException, IOException {
+	// Graphics2D graph = image.createGraphics();
+	// graph.setColor(Color.WHITE);
+	// graph.fillRect(0, 0, image.getWidth(), image.getHeight());
+	// File file = new File("src/main/resources/李旭科漫画体v1.0.ttf");
+	// FileInputStream fis = new FileInputStream(file);
+	// BufferedInputStream bis = new BufferedInputStream(fis);
+	//
+	// Font dynamicFont = Font.createFont(Font.TRUETYPE_FONT, bis).deriveFont(
+	// Font.BOLD, 24f);
+	//
+	// graph.setColor(Color.RED);
+	// graph.setFont(dynamicFont);
+	// graph.drawString(
+	// namecardVo.getNamecardInfo().get(NamecardInfoKeyCons.NICKNAME),
+	// 120, 50);
+	//
+	// graph.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+	// RenderingHints.VALUE_ANTIALIAS_ON);
+	// fis.close();
+	// bis.close();
+	//
+	// graph.setColor(Color.GREEN);
+	// graph.drawString(
+	// namecardVo.getNamecardInfo().get(NamecardInfoKeyCons.COUNTRY),
+	// 120, 100);
+	//
+	// graph.dispose();
+	// }
 
 }
